49b86a4ad9304727ae697183cd39a6e932174834
unknown <michaelieece@yahoo.co.ukgit config --global user.email michaelieece@yahoo.co.ukgit config --global user.email michaelieece@yahoo.co.uk>
2023-05-31 18:14:20 +0200

    message commit

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
             input : {
                 main: 'index.html',
                 bootstrap: 'index-bootstrap.html',
-                materialize: 'materialize.html',
+                materialize: 'index.materialize.html',
             }
         },
     },
